89aea5ec3ee69a937b1b7ad22cc2eeb1ddd1398b
Nadya Cherepaha <n.cherepaha@gmail.com>
2025-11-27 13:38:40 +0400

    fix: apply global body styles in prod

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
-import './app/base.module.css';
+import './app/base.css';
 import './app/theme.css';
 
 createRoot(document.getElementById('root')!).render(
